01f5a670667767050b7859c8302969ae0fa3f94a
wangshibiao <645102170@qq.com>
2020-05-29 18:58:49 +0800

    feat(vuepress/lastUpdated): 文章底部显示最近编辑时间(通过配置参数lastUpdated)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,25 @@ module.exports = {
 
     // 侧边栏,注意, h1标题不会生成在侧边栏
     sidebar: "auto",
-    sidebarDepth: 2
-  }
+    sidebarDepth: 2,
+
+    // 文章底部显示更新时间
+    lastUpdated: "上次更新"
+  },
+
+  plugins: [
+    [
+      /*****************start 自定义"文章底部更新时间"的显示格式 ************/
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp, lang) => {
+          // 不要忘了安装 moment
+          const moment = require("moment");
+          moment.locale(lang);
+          return moment(timestamp).format("YYYY-MM-DD hh:mm:ss");
+        }
+      }
+      /*****************end 自定义lastUpdated的显示格式 ************/
+    ]
+  ]
 };
