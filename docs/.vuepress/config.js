35fc912192b3a11587b587637625c995c38d1224
wangshibiao <645102170@qq.com>
2020-06-02 15:21:44 +0800

    feat(笔记/cdn): 部署免费cdn, 采用方案github+jsdelivr

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,15 @@ module.exports = {
     sidebar: {
       "/笔记/技术/": [
         {
+          title: "前端", // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: [
+            "/笔记/技术/前端/",
+            "/笔记/技术/前端/性能优化/cdn/部署免费cdn"
+          ]
+        },
+        {
           title: "后台", // 必要的
           collapsable: false, // 可选的, 默认值是 true,
           sidebarDepth: 2, // 可选的, 默认值是 1
---
+++ docs/笔记/技术/前端/性能优化/cdn/部署免费cdn.md
@@ -0,0 +1,22 @@
+---
+title: 部署免费cdn
+permalink: /front/performance/cdn/freecdn.html
+---
+
+## 1. 选择方案
+使用github和jsdelivr这对组合, 搭建免费的cdn, 不仅节省存储费用, 还节省带宽费用
+
+::: tip
+使用github的存储, 使用jsdelivr的cdn带宽
+:::
+
+## 2. 部署步骤
+### 2.1 github上创建仓库
+注意是public类型的
+
+### 2.2 使用jsdelivr的cdn地址访问
+将资源的github访问url转换为jsdelivr的cdn url
+
+::: tip
+url转换规则详见https://www.cnblogs.com/zhsh666/p/11432956.html
+:::
