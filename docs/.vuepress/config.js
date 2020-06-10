1ab3c43f33f47d8ce5af2be175a1e09713fa6fdd
wangshibiao <645102170@qq.com>
2020-06-10 18:20:05 +0800

    feat(vuepress/pwa): 支持pwa,但是没有出现"添加到桌面"的弹框提示,原因未知

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -130,8 +130,14 @@ module.exports = {
       }
     ],
 
-    // 全文搜索
-    ["fulltext-search"]
+    // pwa插件
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
   ],
 
   //服务访问路径. 若将服务发布到github pages, 且使用github域名访问, 则必须要配置base, 如base: "/blog/"
@@ -184,6 +190,37 @@ module.exports = {
         async: true,
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
       }
-    ]
+    ],
+
+    /************ start pwa配置项 */
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" }
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c"
+      }
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png"
+      }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+    /************ end pwa配置项 */
   ]
 };
