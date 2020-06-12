c98a9089304f4b0ea8a782bffb0a7d2d471f9abc
wangshibiao <645102170@qq.com>
2020-06-13 01:22:53 +0800

    feat(vuepress/部署): 删除github的自定义域名配置

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yarn docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名, 该操作等同于:前往GitHub Pages页面设置Custom domain
-echo 'blog.sofineday.com' > CNAME
+# echo 'www.sofineday.com' > CNAME
 
 git init
 git add -A
