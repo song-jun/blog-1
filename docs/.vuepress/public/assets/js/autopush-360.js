4a29aabebf9f4bcc52ae24b5543700d38b8aec20
wangshibiao <645102170@qq.com>
2020-06-14 01:16:36 +0800

    fix(vuepress/seo): 百度推送代码和360自动推送代码有语法错误

--- docs/.vuepress/public/assets/js/autopush-360.js
+++ docs/.vuepress/public/assets/js/autopush-360.js
@@ -1,9 +1,6 @@
-// 自动推送站点资源到360, 仅在生产环境安装推送代码，以免造成多域名重复推送问题
-if (process.env.NODE_ENV === "production" && typeof window !== "undefined") {
-  // 如下为从站长平台复制的代码
-  (function() {
-    var src =
-      "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-    document.write('<script src="' + src + '" id="sozz"></script>');
-  })();
-}
+// 最好不要在本地访问页面，以免造成多域名重复推送问题
+(function() {
+  var src =
+    "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+  document.write('<script src="' + src + '" id="sozz"></script>');
+})();
