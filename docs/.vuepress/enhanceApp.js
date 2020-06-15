c394c276e7fac3e41abe0adcb7687f219d546e95
wangshibiao <645102170@qq.com>
2020-06-16 04:00:45 +0800

    fix(vuepress/统计): 切换页面时，没有做cnzz统计

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,11 +5,19 @@ export default ({ router }) => {
   router.beforeEach((to, from, next) => {
     console.log("切换路由", to.fullPath, from.fullPath);
 
-    //触发百度的pv统计
+    //发送百度的pv统计
     if (typeof _hmt != "undefined") {
       if (to.path) {
         _hmt.push(["_trackPageview", to.fullPath]);
         console.log("上报百度统计", to.fullPath);
+      }
+    }
+
+    //发送cnzz的pv统计
+    if (typeof _czc != "undefined") {
+      if (to.path) {
+        _czc.push(["_trackPageview", to.fullPath, from.fullPath]);
+        console.log("上报cnzz统计", to.fullPath);
       }
     }
 
